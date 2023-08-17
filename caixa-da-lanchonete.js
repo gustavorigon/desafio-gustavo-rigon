45f87a8a36296f9eef36bc635ae32e2e067e9614
gustavorigon <141384636+gustavorigon@users.noreply.github.com>
2023-08-16 23:36:17 -0300

    caixa-da-lanchonete.js

--- caixa-da-lanchonete.js
+++ caixa-da-lanchonete.js
@@ -32,12 +32,7 @@ const cardapio = {
         console.log("Quantidade inválida!");
         return 0;
       }
-
-  //Observação: Caso haja um ou mais itens no pedido, o valor total será calculado, 
-  //independente da existência de um item com quantidade zero. Na regra: 
-  //"Se a quantidade de itens for zero, apresentar mensagem "Quantidade inválida!"." 
-  //Não ficou claro se o pedido como um todo deveria ser rejeitado se um item tiver quantidade zero.
-  
+        
       if (!['dinheiro', 'credito', 'debito'].includes(formaPagamento)) {
         console.log("Forma de pagamento inválida!");
         return 0;
